914a9acf6f2e0249105a636a26e98f17972bc421
Milagro García <definitelymili@hotmail.com>
2022-11-07 21:36:44 -0400

    Added forescast information

--- main.js
+++ main.js
@@ -44,6 +44,8 @@ function showTemperature(response) {
   descriptionElement = response.data.weather[0].description;
   iconElement = response.data.weather[0].icon;
   centigradesTempeture = response.data.main.temp;
+  let windValue = document.querySelector("#weather-wind");
+  windValue.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   let temperatureValue = document.querySelector("#temperatureValue");
   temperatureValue.innerHTML = Math.round(centigradesTempeture);
   let weatherDescription = document.querySelector("#weather-description");
@@ -52,7 +54,9 @@ function showTemperature(response) {
   dayAndTime.innerHTML = currentDayAndTime(response.data.dt * 1000);
   let title = document.querySelector(".main-title");
   title.innerHTML = response.data.name;
+  
   showIcon(iconElement);
+  displayForescast(response.data.coord);
 }
 function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
@@ -79,14 +83,15 @@ function inputOnBlur() {
   }
 }
 
-// temperature convertion starts
 function showCentigradesTemperature(event) {
-    event.preventDefault()
-    centigradesLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureValue = document.querySelector("#temperatureValue");
-    temperatureValue.innerHTML = Math.round(centigradesTempeture);
+  event.preventDefault()
+  centigradesLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureValue = document.querySelector("#temperatureValue");
+  temperatureValue.innerHTML = Math.round(centigradesTempeture);
+ 
 }
+
 function showFahrenheitTemperature(event) {
   event.preventDefault();
   centigradesLink.classList.remove("active")
@@ -94,8 +99,10 @@ function showFahrenheitTemperature(event) {
   let temperatureValue = document.querySelector("#temperatureValue")
   temperatureValue.innerHTML = Math.round((centigradesTempeture * 9) / 5 + 32);
 
+  showFahrenheitForescastTemp();
 }
 // temperature convertion ends
+
 // show icon function starts
 function showIcon(iconElement) {
   let icon = document.querySelector("#icon");
@@ -253,6 +260,7 @@ function showIcon(iconElement) {
   screenWidth();
 }
 // show icon function ends
+
 // show animations start
 function showShowerRainAnimation(iconElement) {
   if (iconElement === "09d" || iconElement === "09n") {
@@ -291,18 +299,17 @@ function showMistAnimation(iconElement) {
   }
 }
 // show animations end
+
 // show background starts
 function screenWidth() {
   if (window.matchMedia("(min-width: 432px)").matches) {
     backgroundDAndN();
     backgroundPrecipitations();
     keepBackgroundMobile();
-    console.log("desktop");
   } 
   else {
     mobileBackgroundDAndN();
     mobileBackgroundPrecipitations();
-    console.log("mobile");
     keepBackgroundDesktop();
   }
 }
@@ -453,6 +460,7 @@ function mobileBackgroundPrecipitations() {
 }
    
 // show background ends
+
 // keep the background display in desktop when device width changes *starts*
 function keepBackgroundDesktop() {
     if (mobileBackground.classList.contains("mobile-background-d")) {
@@ -520,6 +528,7 @@ function keepBackgroundDesktop() {
     }
 }
 // keep the background display in desktop when device width changes *ends*
+
 // keep the background display in mobile when device width changes *starts*
 function keepBackgroundMobile() {
   if (desktopBackground.classList.contains("background-day")) {
@@ -580,6 +589,134 @@ function keepBackgroundMobile() {
 }
 // keep the background display in mobile when device width changes *ends*
 
+// forescast starts//
+function displayForescastIcon(iconInformation) {
+  let icon = iconInformation.icon;
+  let description = iconInformation.description;
+  
+  switch (icon) {
+    case "01d":
+        return `<img src="https://i.postimg.cc/3JQVLTQd/sun.png" alt="${description}"></img>`;
+      break;
+
+    case "02d":
+      return `<img src="https://i.postimg.cc/mDv7cKdH/few-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "03d":
+      return `<img src="https://i.postimg.cc/T1qNRwpH/scattered-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "04d":
+      return `<img src="https://i.postimg.cc/FRJXSRgH/broken-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "09d":
+      return `<img src="https://i.postimg.cc/dQHvZGmB/shower-rain.png" alt="${description}"></img>`;
+      break;
+
+    case "10d":
+      return `<img src="https://i.postimg.cc/WbncMgvz/rain.png" alt="${description}"></img>`;
+      break;
+
+    case "11d":
+      return `<img src="https://i.postimg.cc/ZqTxzSNV/thunderstorm.png" alt="${description}"></img>`;
+      break;
+
+    case "13d":
+      return `<img src="https://i.postimg.cc/prGG9SJD/snow.png" alt="${description}"></img>`;
+      break;
+
+    case "50d":
+      return `<img src="https://i.postimg.cc/cLJwHHvt/mist.png" alt="${description}"></img>`;
+      break;
+
+    case "01n":
+      return `<img src="https://i.postimg.cc/3JQVLTQd/sun.png" alt="${description}"></img>`;
+      break;
+
+    case "02n":
+      return `<img src="https://i.postimg.cc/mDv7cKdH/few-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "03n":
+      return `<img src="https://i.postimg.cc/T1qNRwpH/scattered-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "04n":
+      return `<img src="https://i.postimg.cc/FRJXSRgH/broken-clouds.png" alt="${description}"></img>`;
+      break;
+
+    case "09n":
+      return `<img src="https://i.postimg.cc/dQHvZGmB/shower-rain.png" alt="${description}"></img>`;
+      break;
+
+    case "10n":
+      return `<img src="https://i.postimg.cc/WbncMgvz/rain.png" alt="${description}"></img>`;
+      break;
+
+    case "11n":
+      return `<img src="https://i.postimg.cc/ZqTxzSNV/thunderstorm.png" alt="${description}"></img>`;
+      break;
+
+    case "13n":
+      return `<img src="https://i.postimg.cc/prGG9SJD/snow.png" alt="${description}"></img>`;
+      break;
+
+    case "50n":
+      return `<img src="https://i.postimg.cc/cLJwHHvt/mist.png" alt="${description}"></img>`;
+      break;
+  }
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  
+  return days[day]
+
+}
+function forescast(response) {
+    console.log(response)
+    let forescastElement = document.querySelector(".forescast-container");
+
+    let days = response.data.daily;
+
+
+    let forescastHtml = "";
+
+    days.forEach(function(day, index) {
+      if (index < 5) {
+        centigradesTempMax = Math.round(day.temp.max);
+        centigradesTempMin = Math.round(day.temp.min);
+        forescastHtml =
+        forescastHtml +
+         `<div><div class="forescast-day">${formatDay(day.dt)}</div>
+                      <div class="forescast-img">
+                         ${displayForescastIcon(day.weather[0])}
+                      </div>
+                      <div class="forescast-temperature">
+                          <span id="temperature-min">${centigradesTempMax}°</span>
+                          <span id="temperature-max">${centigradesTempMin}°</span>
+                      </div></div>`;
+      }
+    })
+
+     forescastElement.innerHTML = forescastHtml;
+
+}
+function displayForescast(coordinates) {
+  let apiKey = "4b3503b2f08a729413c4d33ef1186004";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(forescast);
+}
+// forescast ends//
+
+let centigradesTemMin = null;
+let centigradesTemMax = null;
 let iconElement = null;
 let descriptionElement = null;
 let centigradesTempeture = null;
